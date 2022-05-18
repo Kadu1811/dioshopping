71b3b82e08f59e00aa1c3cb22757c49ecbcd66a0
Kadu1811 <kadu.dear60@gmail.com>
2022-05-18 18:06:07 -0300

    excluding buttons home and contqato, giving logo /home route, installing whatsapp button no lugar de contato com funcionalidade

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Logo, Button } from './headerstyles';
-import { Link } from 'react-router-dom';
+import { Logo } from './headerstyles';
 import { Grid } from '@material-ui/core/';
 import Cart from './Cart';
 
 const Header = () => {
     return(
         <Grid container direction="row" justify="space-between" alignItems="center" xs={12}>
-            <Logo href="/">STORE</Logo>
-            <Link to="/">
-                <Button>Home</Button>
-            </Link>
-            <Link to="/contato">
-                <Button>Contato</Button>
-            </Link>
+            <Logo href="/">G STORE</Logo>
             <Cart />   
         </Grid>
     )
